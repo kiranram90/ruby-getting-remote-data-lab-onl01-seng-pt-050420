d3c53f69a7c0f6587192e1aeccd9fee632b1ad7b
kiran ram <kram@lions.molloy.edu>
2020-06-19 00:26:09 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,25 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+  
+  def initialize(url)
+    
+  end
+  
+  def get_response_body
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+    
+  end
+  
+  def program_school
+  programs = JSON.parse(self.get_response_body)
+  programs.collect do |program|
+    program["agency"]  
+  end
+end
+ 
+end
