956e451f04421eb80f68274cb9523e07de1b8a0f
kiran ram <kram@lions.molloy.edu>
2020-06-19 01:45:32 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,12 +4,14 @@ require 'json'
 
 class GetRequester
   
+  attr_accessor :url 
+  
   def initialize(url)
-    
+    @url = url
   end
   
   def get_response_body
-    uri = URI.parse(url)
+    uri = URI.parse(@url)
     response = Net::HTTP.get_response(uri)
     response.body
     
